9c8648d17370fb192650b7d2cbabd905e4362c2e
Muazzez Aydın <ayd.muazzez@gmail.com>
2023-03-04 21:03:17 +0300

    created project

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Test</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
