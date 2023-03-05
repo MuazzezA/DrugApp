9e5db9e33896203751350189137df67b79f7eb71
Muazzez Aydın <ayd.muazzez@gmail.com>
2023-03-05 23:30:55 +0300

    add utils

---
+++ app/utils/colors.ts
@@ -0,0 +1,9 @@
+export default {
+    primary: "#2C74B3",
+    secondary: "#BDD8F1",
+    beige: "#D9D9D9",
+    black: "#000000",
+    white: "#FFFFFF",
+    red: "#DC0000",
+    lightGray: "#EDEDED",
+}
---
+++ app/utils/size.ts
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
+
+export default {
+    base: 8,
+    font: 14,
+    radius: 15,
+    padding: 25,
+    icon: 25,
+    WIDTH: width,
+    HEIGHT: height,
+    componentHeight: 45,
+
+}
